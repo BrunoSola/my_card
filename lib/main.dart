aee098a9ab9b474663d9af44c80704dcbd1a4f62
BrunoSola <79088492+BrunoSola@users.noreply.github.com>
2024-09-19 05:19:14 -0300

    Adiciona Cards social medias, githud e email

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+void abrirUrls(String perfilInstagram) async {
+  final Uri urlInstagram = Uri.parse(perfilInstagram);
+  
+  // Verifica se pode abrir o link
+  if (!await canLaunchUrl(urlInstagram)) {
+    await launchUrl(
+      urlInstagram, 
+      mode: LaunchMode.externalApplication, // Tenta abrir o link em um app externo, como o Instagram
+    );
+  } else {
+    debugPrint('Could not launch $urlInstagram');
+  }
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -15,6 +32,7 @@ class MyApp extends StatelessWidget {
         body: Center(
           child: SafeArea(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 const CircleAvatar(
                   backgroundImage: AssetImage('assets/images/perfil_avatar.jpg'),
@@ -23,7 +41,7 @@ class MyApp extends StatelessWidget {
                 const Text(
                   'Bruno dos Reis Sola', 
                   style: TextStyle(
-                    fontSize: 40.0,
+                    fontSize: 30.0,
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
                     fontFamily: 'Pacifico',
@@ -39,31 +57,65 @@ class MyApp extends StatelessWidget {
                     letterSpacing: 2,
                   ),
                 ),
-                Container(
-                  color: Colors.white,
-                  margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
-                  padding: const EdgeInsets.all(10.0),
-                  child: const Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.email,
-                        color: Color.fromRGBO(69, 56, 255, 1),
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
-                        'Brunosola.profissional@gmail.com',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontFamily: 'Roboto',
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 1.5,
-                        ),
-                      ),
-                    ],
+                const SizedBox(
+                  height: 5.0,
+                  width: 280.0, 
+                  child: Divider(
+                    color: Colors.white,
                   ),
                 ),
+                Card( //LinkedinIn
+                  margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+                  child: ListTile(
+                    leading: TextButton.icon(
+                      icon: const FaIcon(FontAwesomeIcons.linkedin),
+                      label: const Text('LinkedinIn',                        
+                      ),
+                      onPressed: () {                        
+                       abrirUrls('https://www.linkedin.com/in/bruno-sola/');
+                      }
+                    ),
+                  ),                  
+                ),
+                Card( //GitHub
+                  margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+                  child: ListTile(
+                    leading: TextButton.icon(
+                      icon: const FaIcon(FontAwesomeIcons.github),
+                      label: const Text('GitHub',                        
+                      ),
+                      onPressed: () {                        
+                       abrirUrls('https://github.com/BrunoSola');
+                      }
+                    ),
+                  ),                  
+                ),
+                Card( //Instagram
+                  margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+                  child: ListTile(
+                    leading: TextButton.icon(
+                      icon: const FaIcon(FontAwesomeIcons.instagram),
+                      label: const Text('Instagram',                        
+                      ),
+                      onPressed: () {                        
+                       abrirUrls('https://www.instagram.com/bruno_rsola');
+                      }
+                    ),
+                  ),
+                ),
+                Card( //E-mail
+                  margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+                  child: ListTile(
+                    leading: TextButton.icon(
+                      icon: const Icon(Icons.email),
+                      label: const Text('E-mail'),
+                      onPressed: () {
+                        abrirUrls('mailto:brunosola.profissional@gmail.com');
+                      }
+                    ),
+                  ),
+                ),
+ 
               ]
             ),
           ),
